7593f830606e223bf70c534f260823c5193d92a4
yellekwt <taylor.w.kelley@gmail.com>
2017-01-12 22:42:23 -0500

    Added new functional test for unique urls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -54,10 +54,53 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-		# Edith wonders whether the site will remember her list. Then she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect.
-		self.fail('Finish the test!')
-		# She visits that URL - her to-do list is still there.
-
 		# Satisfied, she goes back to sleep
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# Edith starts a new todo list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		# She notices that here list has a unique URL
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+		# Now a new user, Francis, comes along to the site.
+
+		##We use a new browser session to make sure that no information
+		## of Edith's is coming through from cookies etc
+		self.browser.quit()
+		self.browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
+		firefox_path='/Users/taylorkelley/testgoat/Firefox.app/Contents/MacOS/firefox'
+		))
+
+		# Francis visits the home page. There is no sign of Edith's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		assertNotIn('Buy peacock feathers', page_text)
+		assertNotIn('make a fly', page_text)
+
+		# Francis starts a new list by entering a new item. He
+		# is less interesting than Edith...
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy Milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy Milk')
+
+		# Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url,edith_list_url)
+
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		assertNotIn('Buy peacock feathers', page_text)
+		assertNotIn('make a fly', page_text)
+
+		# Satisfied, they both go back to sleep
+
+
+		
