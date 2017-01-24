fe07fdf257179f3d3183352213e5ef5e74191a91
yellekwt <taylor.w.kelley@gmail.com>
2017-01-23 22:04:35 -0500

    Fixed functional tests, added new URL,view, and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -80,8 +80,8 @@ class NewVisitorTest(LiveServerTestCase):
 		# Francis visits the home page. There is no sign of Edith's list
 		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
-		assertNotIn('Buy peacock feathers', page_text)
-		assertNotIn('make a fly', page_text)
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
 
 		# Francis starts a new list by entering a new item. He
 		# is less interesting than Edith...
@@ -97,8 +97,8 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# Again, there is no trace of Edith's list
 		page_text = self.browser.find_element_by_tag_name('body').text
-		assertNotIn('Buy peacock feathers', page_text)
-		assertNotIn('make a fly', page_text)
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
 
 		# Satisfied, they both go back to sleep
 
